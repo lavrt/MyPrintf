f4dbed3b68f22508a2a727abd04d312248c5857b
lavrt <lavrik-aleksey-1@yandex.ru>
2025-03-28 00:05:20 +0300

    header

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
-extern "C" int my_printf_t(const char* format, ...);  // to header
-                                                // shall script
-                                                // в readme сигнатуру функции добавить
+#include "my_printf.h"
 
 int main() {
-    // my_printf_t(
-    //     "This is my printf function and it can output:\n"
-    //     "string >> %s\n"
-    //     "char   >> %c\n"
-    //     "dec    >> %d\n"
-    //     "hex    >> %x\n"
-    //     "oct    >> %o\n"
-    //     "bin    >> %b\n"
-    //     "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d \n\n",
+    my_printf_t(
+        "This is my printf function and it can output:\n"
+        "string >> %s\n"
+        "char   >> %c\n"
+        "dec    >> %d\n"
+        "hex    >> %x\n"
+        "oct    >> %o\n"
+        "bin    >> %b\n"
+        "\n",
 
-    //     "hello world!", '$', 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044);
+        "hello world!", '$', 2025, 2025, 2025, 2025);
     
-    my_printf_t("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n", -1, -1, "love", 3802, 100, 33, 127, -1, "love", 3802, 100, 33, 127);
-
+    my_printf_t(
+        "Test output:\n"
+        "%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n",
+        -1, -1, "love", 3802, 100, 33, 127, -1, "love", 3802, 100, 33, 127);
 
     return 0;
 }
---
+++ my_printf.h
@@ -0,0 +1,6 @@
+#ifndef MY_PRINTF_H
+#define MY_PRINTF_H
+
+extern "C" int my_printf_t(const char* format, ...);
+
+#endif // MY_PRINTF_H
