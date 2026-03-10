0b9fd4b90ad915d7655efbb1dc21700b70d7a5b9
Alexey <lavrik-aleksey-1@yandex.ru>
2026-03-10 13:15:23 +0300

    update example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "my_printf.h"
 
+#include <stdio.h>
+
 int main() {
-    my_printf_t(
-        "This is my printf function and it can output:\n"
+    printf(
+        "-------------------------------------\n"
+        "This is default C printf function:\n"
+        "-------------------------------------\n\n"
+    );
+
+    printf(
         "string >> %s\n"
         "char   >> %c\n"
         "dec    >> %d\n"
@@ -10,13 +17,37 @@ int main() {
         "oct    >> %o\n"
         "bin    >> %b\n"
         "\n",
+        "hello world!", '$', 2025, 2025, 2025, 2025
+    );
 
-        "hello world!", '$', 2025, 2025, 2025, 2025);
+    printf(
+        "Test output:\n"
+        "%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n",
+        -1, -1, "love", 3802, 100, 33, 127, -1, "love", 3802, 100, 33, 127
+    );
+
+    my_printf_t(
+        "\n-------------------------------------\n"
+        "This is my printf function:\n"
+        "-------------------------------------\n\n"
+    );
+
+    my_printf_t(
+        "string >> %s\n"
+        "char   >> %c\n"
+        "dec    >> %d\n"
+        "hex    >> %x\n"
+        "oct    >> %o\n"
+        "bin    >> %b\n"
+        "\n",
+        "hello world!", '$', 2025, 2025, 2025, 2025
+    );
     
     my_printf_t(
         "Test output:\n"
         "%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n",
-        -1, -1, "love", 3802, 100, 33, 127, -1, "love", 3802, 100, 33, 127);
+        -1, -1, "love", 3802, 100, 33, 127, -1, "love", 3802, 100, 33, 127
+    );
 
     return 0;
 }
